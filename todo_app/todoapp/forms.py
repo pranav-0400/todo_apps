d6dbcd29d21ebfe4848ca4c905d15c037f52590d
pranav-0400 <145419867+pranav-0400@users.noreply.github.com>
2024-04-17 11:51:07 +0530

    Delete todo_app directory

--- todo_app/todoapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms 
-from .models import Task 
-
-
-class TaskForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={"class":"myinput", "placeholder":"Enter Todo"}))
-    class Meta:
-        model = Task 
-        fields = ["title"]
